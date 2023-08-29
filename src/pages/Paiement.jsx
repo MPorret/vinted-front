1c6d0d7458771e9fb2058108ce4fdfd947534e5d
Marine Porret <marine.porret@gmail.com>
2023-08-29 18:11:51 +0200

    Paiement if connected only

--- src/pages/Paiement.jsx
+++ src/pages/Paiement.jsx
@@ -21,45 +21,76 @@ const Paiement = ({ token }) => {
 
   return (
     <main className="pay">
-      <section>
-        <h2>Résumé de la commande</h2>
-        <div>
-          <span>Prix de l'article</span>
-          <span>{productPrice} €</span>
-        </div>
-        <div>
-          <span>Frais protection acheteurs</span>
-          <span>{(productPrice / 100).toFixed(2)} €</span>
-        </div>
-        <div>
-          <span>Frais de port</span>
-          <span>{(productPrice / 50).toFixed(2)} €</span>
-        </div>
-      </section>
-      <section>
-        <div className="bold">
-          <span>Total</span>
-          <span>{total} €</span>
-        </div>
-      </section>
-      <section>
-        <p>
-          Il ne vous reste plus qu'une étape pour vous offrir{" "}
-          <span className="bold">{name}</span>. Vous allez payer{" "}
-          <span className="bold">{total} €</span> (frais de protection et frais
-          de port inclus).
-        </p>
-      </section>
-      <section>
-        <Elements stripe={stripePromise}>
-          <CheckoutForm
-            token={token}
-            price={total}
-            name={name}
-            productId={productId}
-          />
-        </Elements>
-      </section>
+      {token ? (
+        <>
+          <section>
+            <h2>Résumé de la commande</h2>
+            <div>
+              <span>Prix de l'article</span>
+              <span>{productPrice} €</span>
+            </div>
+            <div>
+              <span>Frais protection acheteurs</span>
+              <span>{(productPrice / 100).toFixed(2)} €</span>
+            </div>
+            <div>
+              <span>Frais de port</span>
+              <span>{(productPrice / 50).toFixed(2)} €</span>
+            </div>
+          </section>
+          <section>
+            <div className="bold">
+              <span>Total</span>
+              <span>{total} €</span>
+            </div>
+          </section>
+          <section>
+            <p>
+              Il ne vous reste plus qu'une étape pour vous offrir{" "}
+              <span className="bold">{name}</span>. Vous allez payer{" "}
+              <span className="bold">{total} €</span> (frais de protection et
+              frais de port inclus).
+            </p>
+          </section>
+          <section>
+            <Elements stripe={stripePromise}>
+              <CheckoutForm
+                token={token}
+                price={total}
+                name={name}
+                productId={productId}
+              />
+            </Elements>
+          </section>
+        </>
+      ) : (
+        <section className="notconnected">
+          <h2>Tu dois être connecté pour acheter un article.</h2>
+          <section className="form">
+            <button
+              onClick={() => {
+                isVisible();
+              }}
+              disabled={visible}
+            >
+              S'inscrire
+            </button>
+            <button
+              onClick={() => {
+                isVisible();
+              }}
+              disabled={!visible}
+            >
+              Se connecter
+            </button>
+          </section>
+          {visible ? (
+            <SignUp handleToken={handleToken} isVisible={isVisible} />
+          ) : (
+            <LogIn handleToken={handleToken} isVisible={isVisible} />
+          )}
+        </section>
+      )}
     </main>
   );
 };
